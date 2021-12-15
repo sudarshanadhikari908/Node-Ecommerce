8c3fabbf7a60a245156b1aa23f99ff8d0ddae2c7
sudan <amstersudhan633@gmail.com>
2021-12-15 20:04:36 +0545

    Adding express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.get("/", function (req, res) {
+  return res.send("hello");
+});
+
+app.listen(3000, function () {
+  console.log("Server running on port 3000");
+});
